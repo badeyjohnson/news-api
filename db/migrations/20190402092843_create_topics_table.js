d6b059d525667d346bc1d43bded1dd0c3ebd1193
badeyjohnson <ben.adey-johnson@arup.com>
2019-04-02 09:47:18 +0100

    create topics table migration

---
+++ db/migrations/20190402092843_create_topics_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  console.log('creating topics table...');
+  return knex.schema.createTable('topics', (topicsTable) => {
+    topicsTable.string('topic_name').notNullable().primary().unique();
+    topicsTable.string('topic_description').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log('removing topics tables...');
+  return knex.schema.dropTable('topics');
+};
--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS news-api;
-CREATE DATABASE news-api;
+DROP DATABASE IF EXISTS "news-api";
+CREATE DATABASE "news-api";
 
-DROP DATABASE IF EXISTS news-api_test;
-CREATE DATABASE news-api_test;
+DROP DATABASE IF EXISTS "news-api_test";
+CREATE DATABASE "news-api_test";
