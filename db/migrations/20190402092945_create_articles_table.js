ae1f1429fc38a48bbcb821463788e8163c8d2a8c
badeyjohnson <ben.adey-johnson@arup.com>
2019-04-02 10:25:07 +0100

    type revisions in table migrations

--- db/migrations/20190402092945_create_articles_table.js
+++ db/migrations/20190402092945_create_articles_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   console.log('creating articles table...');
   return knex.schema.createTable('articles', (articlesTable) => {
-    articlesTable.increment('article_id').notNullable().primary();
+    articlesTable.increments('article_id').notNullable().primary();
     articlesTable.string('article_title').notNullable();
     articlesTable.text('article_body').notNullable();
     articlesTable.integer('article_votes').notNullable().defaultTo(0);
--- db/migrations/20190402092953_create_comments_table.js
+++ db/migrations/20190402092953_create_comments_table.js
@@ -2,11 +2,11 @@
 exports.up = function(knex, Promise) {
   console.log('creating comments table...');
   return knex.schema.createTable('comments', (commentsTable) => {
-    commentsTable.increment('comment_id').notNullable().primary();
+    commentsTable.increments('comment_id').notNullable().primary();
     commentsTable.text('comment_body').notNullable();
     commentsTable.integer('comment_votes').notNullable().defaultTo(0);
     commentsTable.string('comment_author').references('user_username').inTable('users');
-    commentsTable.string('comment_article_id').references('article_id').inTable('articles');
+    commentsTable.integer('comment_article_id').references('article_id').inTable('articles');
     commentsTable.timestamp('comment_created_at').notNullable().defaultTo(knex.fn.now());
   });
 };
