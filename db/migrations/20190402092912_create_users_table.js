88a87fc223a2590473bfddb9a212cceb269a6e4c
badeyjohnson <ben.adey-johnson@arup.com>
2019-04-02 09:50:11 +0100

    create users table migration

--- db/migrations/20190402092912_create_users_table.js
+++ db/migrations/20190402092912_create_users_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  console.log('creating users table...');
+  return knex.schema.createTable('users', (usersTable) => {
+    usersTable.string('user_username').notNullable().primary().unique();
+    usersTable.string('user_avatar_url').notNullable().unique();
+    usersTable.string('user_name').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  console.log('removing users tables...');
+  return knex.schema.dropTable('users');
 };
