75dfd38060bef3a7b828ea59f55ba9ea8d0ab8e3
badeyjohnson <ben.adey-johnson@arup.com>
2019-04-02 10:15:31 +0100

    create comments table migration

--- db/migrations/20190402092953_create_comments_table.js
+++ db/migrations/20190402092953_create_comments_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  console.log('creating comments table...');
+  return knex.schema.createTable('comments', (commentsTable) => {
+    commentsTable.increment('comment_id').notNullable().primary();
+    commentsTable.text('comment_body').notNullable();
+    commentsTable.integer('comment_votes').notNullable().defaultTo(0);
+    commentsTable.string('comment_author').references('user_username').inTable('users');
+    commentsTable.string('comment_article_id').references('article_id').inTable('articles');
+    commentsTable.timestamp('comment_created_at').notNullable().defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  console.log('removing comments tables...');
+  return knex.schema.dropTable('comments');
 };
